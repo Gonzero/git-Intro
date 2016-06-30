c1c86189a6943992dcce19b6350bade23f40be2f
Sean Lehman <sjlehman4@comcast.net>
2016-06-30 12:25:30 -0400

    Middle of the challenge

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,26 @@
 // Create a variable called msg to hold a new message
-var msg = 'Sign up to receive our newsletter for 10% off!';
+ // var msg = 'Sign up to receive our newsletter for 10% off!';
 ​
 // Create a function to update the content of the element whose id attribute has a value of message
+
+var msg1 = "I hope to get a good understanding of how to create web pages using concepts and knowledge of coding."
+var msg2 = "I want to be adept at switching between different languages and interfaces to complete a project"
+var msg3 = "I want to have experience working in a team environment"
+
 function updateMessage() {
 ​
-​
-  var el = document.getElementById('message');
-​
-​
-  el.textContent = msg;
+​var el1 = document.getElementById("message1");
+var el2 = document.getElementById("message2");
+var el3 = document.getElementById("message3");
+
+
+el1.textContent = msg1;
+el2.textContent = msg2;
+el3.textContent = msg3;
 }
 ​
 // Call the function
 updateMessage();
+
+
+
