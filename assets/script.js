484b08d3053c591f9229154373bfc44153a8e382
Sean Lehman <sjlehman4@comcast.net>
2016-06-30 14:04:33 -0400

    Middle of the challenge 3

--- assets/script.js
+++ assets/script.js
@@ -3,27 +3,25 @@
 ​
 // Create a function to update the content of the element whose id attribute has a value of message
 
-// var msg1 = "I hope to get a good understanding of how to create web pages using concepts and knowledge of coding."
-// var msg2 = "I want to be adept at switching between different languages and interfaces to complete a project"
-// var msg3 = "I want to have experience working in a team environment"
+var msg1 = "I hope to get a good understanding of how to create web pages using concepts and knowledge of coding."
+var msg2 = "I want to be adept at switching between different languages and interfaces to complete a project"
+var msg3 = "I want to have experience working in a team environment"
 
-// function updateMessage() {
-// ​
-// ​var el1 = document.getElementById("message1")
-// el1.textContent = msg1
+function updateMessage() {
+​
+​var el1 = document.getElementById("message1")
+el1.textContent = msg1
 
-// var el2 = document.getElementById("message2")
-// el2.textContent = msg2
+var el2 = document.getElementById("message2")
+el2.textContent = msg2
 
-// var el3 = document.getElementById("message3")
-// el3.textContent = msg3
+var el3 = document.getElementById("message3")
+el3.textContent = msg3
 
-
-
-// }
+}
 // ​
 // // Call the function
-// updateMessage();
+updateMessage();
 
 
 // Create a variable called el to hold the element whose id attribute has a value of info
